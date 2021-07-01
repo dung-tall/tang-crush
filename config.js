f2184c5b67fb866724c23c9ec8acdb500bae6264
dung-tall <86700817+dung-tall@users.noreply.github.com>
2021-07-01 11:00:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
     introTitle: 'Anh ưi !',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim aem lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'muốm đấm nhau cùng mik mỗi ngày khum 😙',
@@ -11,7 +11,7 @@ const CONFIG = {
     btnNo: 'Không,emmmm à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại đấm nhau với em <3',
     btnReply: 'Gửi cho em :(( <3',
-    reply: 'đấm là yeu òi :((((((((((((((((((((((((((((((((((((((((((((((((((((((  <33333333',
+    reply: 'đấm là yeu òi đóoooo:((((((((((((((((((((((((((((((((((((((((((((((((((((((  <33333333',
     mess: ' hishi biết mò ',
     messDesc: 'tối nay call hát nghe nhó .',
     btnAccept: 'Okiiiii lun <3',

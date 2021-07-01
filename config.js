c2c774299de2f6e533fb0a82e448dc79b8a582f9
dung-tall <86700817+dung-tall@users.noreply.github.com>
2021-07-01 11:05:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'muốm đấm nhau cùng mik mỗi ngày khum 😙',
-    desc: 'babe ',
+    desc: 'khong trả lời mà thoát ra thì mikk yeu nhau mẹ đi:(( ',
     btnYes: 'Vẫn cứ là thích đấm em  <33',
     btnNo: 'Không,emmmm à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại đấm nhau với em <3',
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: ' hishi biết mò ',
     messDesc: 'tối nay call hát nghe nhó .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/dung.nguyenthuy.315424'
+    messLink: 'https://www.facebook.com/dung.nguyenthuy.3154284'
 }
